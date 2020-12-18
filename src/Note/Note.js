d05efdf956f71c5e2913bf27a853b88f44e4fb9d
Lara Samuelson <lara.samuelson@gmail.com>
2020-12-18 06:06:36 -0800

    fix bug page reload

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -30,8 +30,8 @@ export default class Note extends React.Component{
         })
         .then(() => {
             this.context.deleteNote(noteId)
-            // allow parent to perform extra behaviour
             this.props.onDeleteNote(noteId)
+            this.props.history.push(`/`)
         })
         .catch(error => {
             console.error({ error })
